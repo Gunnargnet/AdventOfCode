fb3bf25e162b05259d2ca9d1f81224aa57f3e9e3
Gunnar Söderberg <gunnar.soderberg@cgi.com>
2023-11-29 14:55:11 +0100

    InitialCommit2

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, World!");
+var helper = 0;
+
+Console.WriteLine(helper);
