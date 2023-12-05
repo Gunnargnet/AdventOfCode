763b5ee2e1cd98aae4bbb3fd6e078ebff5553ae9
Gunnar Söderberg <gunnar.soderberg@cgi.com>
2023-12-05 16:35:42 +0100

    Day1

---
+++ AocUtils/Utils.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AocUtils;
+
+public static class Utils
+{
+
+	public static string[] GetImportedData(string filename = "TextFile1.txt")
+	{
+		var allLines = File.ReadAllLines(filename);
+
+
+		//StreamReader sr = new StreamReader(filename);
+
+		//var line = sr.ReadLine();
+
+		////Continue to read until you reach end of file
+		//while (line != null)
+		//{
+		//	//write the line to console window
+		//	Console.WriteLine(line);
+		//	//Read the next line
+		//	line = sr.ReadLine();
+		//}
+		////close the file
+		//sr.Close();
+
+		return allLines;
+
+	}
+
+}
+
+
+
--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,5 +1,35 @@
-﻿var utils = new Day1.Utils();
+﻿using AocUtils;
 
-var test = utils.GetImportedData("NewFile");
+var lines = Utils.GetImportedData("day1.txt");
+var total2star = 0;
+var total1star = 0;
 
-Console.WriteLine(test);
+foreach (var line in lines)
+{
+    var realString = line;
+
+    realString = realString.Replace("one", "one1one");
+    realString = realString.Replace("two", "two2two");
+    realString = realString.Replace("three", "three3three");
+    realString = realString.Replace("four", "four4four");
+    realString = realString.Replace("five", "five5five");
+    realString = realString.Replace("six", "six6six");
+    realString = realString.Replace("seven", "seven7seven");
+    realString = realString.Replace("eight", "eight8eight");
+    realString = realString.Replace("nine", "nine9nine");
+
+    var result1star = line.Where(x => char.IsDigit(x)).ToList();
+    var stringNumber1star = result1star.First().ToString() + result1star.Last().ToString();
+
+    var result2star = realString.Where(x => char.IsDigit(x)).ToList();
+    var stringNumber2star = result2star.First().ToString() + result2star.Last().ToString();
+
+    total1star += int.Parse(stringNumber1star);
+    total2star += int.Parse(stringNumber2star);
+}
+
+Console.WriteLine(total1star);
+Console.WriteLine(total2star);
+
+
+
